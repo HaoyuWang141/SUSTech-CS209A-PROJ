b142c74bbdb46df44770e4de2e2adff2ea516ad3
Haoyu Wang <haoyu.wang141@outlook.com>
2023-05-18 18:21:11 +0800

    config remote database

--- javaProject/src/main/java/sustech/project/javaproject/controller/AnswersController.java
+++ javaProject/src/main/java/sustech/project/javaproject/controller/AnswersController.java
@@ -1,7 +1,6 @@
 package sustech.project.javaproject.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
