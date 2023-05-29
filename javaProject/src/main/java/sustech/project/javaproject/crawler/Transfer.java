933ee6c302080372302f0fc4a93357c7da03d2c7
YiyuLiu-Rin <rinland@qq.com>
2023-05-29 22:08:40 +0800

    .

---
+++ javaProject/src/main/java/sustech/project/javaproject/crawler/Transfer.java
@@ -0,0 +1,28 @@
+package sustech.project.javaproject.crawler;
+
+import com.google.gson.Gson;
+import sustech.project.javaproject.crawler_model.JsonQuestion;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transfer {
+
+    static JsonQuestion[] transferQuestion() throws FileNotFoundException {
+        Gson gson = new Gson();
+        BufferedReader in = new BufferedReader(new FileReader("./src/main/resources/data/questions1.json"));
+        JsonQuestion[] questions = gson.fromJson(in, JsonQuestion[].class);
+        return questions;
+    }
+
+    static JsonAnswer[] transferAnswer() throws FileNotFoundException {
+        Gson gson = new Gson();
+        BufferedReader in = new BufferedReader(new FileReader("./src/main/resources/data/answers1.json"));
+        JsonAnswer[] answers = gson.fromJson(in, JsonAnswer[].class);
+        return answers;
+    }
+
+}
